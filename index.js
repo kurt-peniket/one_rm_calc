1cf34c78cb18d8275af4d69db1d06077e8dc508e
kurt-peniket <64552537+kurt-peniket@users.noreply.github.com>
2020-08-15 17:55:42 +0100

    new changes

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const btn = document.getElementById('btn');
 
 btn.addEventListener('click', function() {
     let n = document.getElementById('n').value;
+    document.getElementById('resultN').style.display = 'flex';
 
     return n == !isNaN ? document.getElementById('resultN').textContent = ('Please enter a number') 
-    : document.getElementById('resultN').textContent = (`You can do aproximately ${(n * 1.1307 + 0.6998).toFixed(1)} for one rep!`);
+    : document.getElementById('resultN').textContent = (`${(n * 1.1307 + 0.6998).toFixed(1)}kgs`);
 })
+
+// You can do aproximately ${(n * 1.1307 + 0.6998).toFixed(1)}kg for one rep!
